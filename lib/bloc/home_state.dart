45fa6c8fb7e4d886d8fefc91282f89525a714aae
Issah Asiedu <issahdundason@gmail.com>
2023-01-03 21:45:50 +0000

    add transactions to home state

--- lib/bloc/home_state.dart
+++ lib/bloc/home_state.dart
@@ -1,19 +1,22 @@
 import 'package:equatable/equatable.dart';
 
+import '../model/expenditure.dart';
 import '../model/product_type.dart';
 import '../service/database.dart';
 
 class HomeState extends Equatable {
   final Financials? currentFinancials;
   final List<MonthSpending> monthExpenditures;
+  final List<Expenditure> transactionsToday;
 
-  const HomeState({this.currentFinancials, this.monthExpenditures = const []});
+  const HomeState({this.currentFinancials, this.monthExpenditures = const [], this.transactionsToday = const[]});
 
-  HomeState copyWith({Financials? financials, List<MonthSpending>? monthSpending}) {
+  HomeState copyWith(
+      {Financials? financials, List<MonthSpending>? monthSpending, List<Expenditure>? transactions}) {
     return HomeState(
         currentFinancials: financials ?? currentFinancials,
-      monthExpenditures: monthSpending ?? monthExpenditures
-        );
+        monthExpenditures: monthSpending ?? monthExpenditures,
+    transactionsToday: transactions ?? transactionsToday);
   }
 
   @override
