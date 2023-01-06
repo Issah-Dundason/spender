63e671c7fb6eb74526edd9276eecde23445a2a4f
Issah Asiedu <issahdundason@gmail.com>
2023-01-06 01:52:17 +0000

    add slide transition to page navigation

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -3,13 +3,15 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spender/pages/profile_page.dart';
 
 class TopBar {
-  static AppBar getAppBar(BuildContext context, String text, String assetName) => AppBar(
+  static AppBar getAppBar(
+          BuildContext context, String text, String assetName) =>
+      AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         elevation: 0,
         centerTitle: true,
         leading: GestureDetector(
           onTap: () {
-            Navigator.pushNamed(context, AppProfile.routeName);
+            Navigator.of(context).push(_createRoute());
           },
           child: Padding(
             padding: const EdgeInsets.only(left: 8.0, top: 4, bottom: 4),
@@ -24,6 +26,20 @@ class TopBar {
         title: Text(
           text,
         ),
-    foregroundColor: Colors.black,
+        foregroundColor: Colors.black,
       );
+
+  static Route _createRoute() {
+    return PageRouteBuilder(
+        pageBuilder: (context, anim, anim2) => const AppProfile(),
+    transitionsBuilder: (context, anim, anim2, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          final tween = Tween(begin: begin, end: end);
+          final offsetAnimation = anim.drive(tween);
+      return SlideTransition(
+        position: offsetAnimation,
+          child: child);
+    });
+  }
 }
