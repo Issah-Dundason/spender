3979bdf7182d8681f6d3dbdb8bce68e6dd22961e
Issah-Dundason <issahdundason@gmail.com>
2023-02-14 07:29:17 +0000

    refactor expenses bloc and add some test

--- test/bloc/expenses_test.dart
+++ test/bloc/expenses_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:spender/bloc/expenses/expenses_bloc.dart';
 import 'package:spender/bloc/expenses/expenses_event.dart';
@@ -6,41 +5,109 @@ import 'package:spender/bloc/expenses/expenses_state.dart';
 import 'package:spender/model/bill.dart';
 import 'package:spender/repository/expenditure_repo.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:bloc_test/bloc_test.dart';
 
 import '../database/test_database.dart';
 
 void main() async {
   sqfliteFfiInit();
-  TestDatabaseClient dbClient = await TestDatabaseClient().init();
+  late TestDatabaseClient dbClient;
 
-  test('gets events by date', () async {
-
-    //Arrange
-    AppRepository repo = AppRepository(dbClient);
-    ExpensesBloc sut = ExpensesBloc(appRepo: repo);
-
-    var types = await repo.getBillTypes();
-
-    var bill = Bill.fromJson({
-      'title': 'Bill',
-      'priority': 'need',
-      'payment_type': 'cash',
-      'pattern': 1,
-      'payment_datetime': '2023-02-12T23:00:00.000',
-      'amount': 200,
-      'end_date': '2023-02-13T23:59:00.000',
-      'type': types[0].toJson()
-    });
-    await repo.saveBill(bill);
-    int? id;
-    late List<Object> savedBill;
-
-    sut.stream.listen((event) {
-      print(event);
-    });
-
-    sut.add(ChangeDateEvent(DateTime.parse('2023-02-12')));
+  setUpAll(() async {
+    dbClient = await TestDatabaseClient().init();
   });
 
+  group('can retrieve bills', () {
+    blocTest(
+      'can retrieve generated bill by date',
+      build: () {
+        AppRepository repo = AppRepository(dbClient);
+        return ExpensesBloc(appRepo: repo);
+      },
+      act: (bloc) => bloc.add(ChangeDateEvent(DateTime.parse('2023-02-06'))),
+      wait: const Duration(milliseconds: 5),
+      skip: 1,
+      expect: () {
+        return [
+          predicate<ExpensesState>((state) {
+            var bill = state.transactions[0];
+            return bill.isGenerated && bill.isRecurring;
+          })
+        ];
+      },
+    );
 
+    blocTest(
+      'can retrieve saved recurring bill by date',
+      build: () {
+        AppRepository repo = AppRepository(dbClient);
+        return ExpensesBloc(appRepo: repo);
+      },
+      act: (bloc) => bloc.add(ChangeDateEvent(DateTime.parse('2023-02-05'))),
+      wait: const Duration(milliseconds: 5),
+      skip: 1,
+      expect: () {
+        return [
+          predicate<ExpensesState>((state) {
+            var bill = state.transactions[0];
+            return !bill.isGenerated && bill.isRecurring;
+          })
+        ];
+      },
+    );
+
+    blocTest(
+      'can retrieve saved non recurring bill by date',
+      build: () {
+        AppRepository repo = AppRepository(dbClient);
+        return ExpensesBloc(appRepo: repo);
+      },
+      act: (bloc) => bloc.add(ChangeDateEvent(DateTime.parse('2023-02-13'))),
+      wait: const Duration(milliseconds: 5),
+      skip: 1,
+      expect: () {
+        return [
+          predicate<ExpensesState>((state) {
+            var bill = state.transactions[0];
+            return !bill.isGenerated &&
+                !bill.isRecurring &&
+                bill.endDate == null;
+          })
+        ];
+      },
+    );
+  });
+
+  group('can delete bills', () {
+    late Bill firstTestBill;
+
+    blocTest('can delete non recurring bill',
+        setUp: () async {
+          var repo = AppRepository(dbClient);
+          firstTestBill = (await repo.getAllBills('2023-05-01')).first;
+        },
+        build: () {
+          var repo = AppRepository(dbClient);
+          return ExpensesBloc(appRepo: repo);
+        },
+        wait: const Duration(milliseconds: 5),
+        act: (bloc) {
+          bloc.add(BillDeleteEvent(bill: firstTestBill));
+        },
+        expect: () {
+          return [
+            predicate<ExpensesState>((state) {
+              return state.deleteState == DeleteState.deleting;
+            }),
+            predicate<ExpensesState>((state) {
+              return state.deleteState == DeleteState.deleted;
+            }),
+          ];
+        }, verify: (bloc) {
+          var repo = AppRepository(dbClient);
+          repo.getAllBills('2023-05-01').then(expectAsync1((bills) {
+            expect(true, equals(bills.isEmpty));
+          }));
+        });
+  });
 }
--- test/database/test_database.dart
+++ test/database/test_database.dart
@@ -1,4 +1,6 @@
 import 'package:path_provider/path_provider.dart';
+import 'package:spender/model/bill.dart';
+import 'package:spender/repository/expenditure_repo.dart';
 import 'package:spender/service/database.dart';
 import 'package:spender/service/queries.dart';
 import 'package:sqflite/sqflite.dart';
@@ -6,6 +8,64 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class TestDatabaseClient extends DatabaseClient {
   static const String _databaseName = "spenderTest.app";
+
+  //add types before saving
+  var testJson = [
+    {
+      'title': 'Bill 1',
+      'priority': 'need',
+      'payment_type': 'cash',
+      'pattern': 1,
+      'payment_datetime': '2023-01-01T23:00:00.000',
+      'amount': 200,
+      'end_date': '2023-01-10T23:59:00.000',
+    },
+    //for testing recurring bill retrieval
+    {
+      'title': 'Bill 2',
+      'priority': 'need',
+      'payment_type': 'cash',
+      'pattern': 1,
+      'payment_datetime': '2023-02-05T23:00:00.000',
+      'amount': 200,
+      'end_date': '2023-02-10T23:59:00.000',
+    },
+    //for testing non recurring bill retrieval,
+    {
+      'title': 'Bill 3',
+      'priority': 'need',
+      'payment_type': 'cash',
+      'pattern': 0,
+      'payment_datetime': '2023-02-13T23:00:00.000',
+      'amount': 200,
+    },
+    {
+      'title': 'Bill 4',
+      'priority': 'want',
+      'payment_type': 'cash',
+      'pattern': 0,
+      'payment_datetime': '2023-03-01T23:00:00.000',
+      'amount': 200,
+    },
+    {
+      'title': 'Bill 5',
+      'priority': 'need',
+      'payment_type': 'cash',
+      'pattern': 1,
+      'payment_datetime': '2023-04-12T23:00:00.000',
+      'amount': 200,
+      'end_date': '2023-04-15T23:59:00.000',
+    },
+    //for testing non recurring bill deletion
+    {
+      'title': 'Bill 6',
+      'priority': 'want',
+      'payment_type': 'cash',
+      'pattern': 0,
+      'payment_datetime': '2023-05-01T23:00:00.000',
+      'amount': 200,
+    }
+  ];
 
   @override
   Future<TestDatabaseClient> init() async {
@@ -15,15 +75,19 @@ class TestDatabaseClient extends DatabaseClient {
 
   Future<void> _openDatabase() async {
     var dir = await getApplicationDocumentsDirectory();
-    db = await databaseFactoryFfi.openDatabase(inMemoryDatabasePath);
-    _create(db);
+    db = await databaseFactoryFfi.openDatabase("${dir.path}/$_databaseName");
+    await _create(db);
   }
 
-  void _create(Database db) async {
+  Future<void> _create(Database db) async {
     await db.execute(Query.billTypeTable);
     await db.execute(Query.expenditureTable);
     await db.execute(Query.budgetTable);
     await db.execute(Query.expenditureExceptionTable);
+
+    await db.delete('bill_type');
+    await db.delete('expenditure');
+    await db.delete('budget');
 
     await db.insert("bill_type", {"name": "Food", "image": "food.svg"});
     await db.insert(
@@ -36,6 +100,15 @@ class TestDatabaseClient extends DatabaseClient {
     await db.insert(
         "bill_type", {"name": "Transportation", "image": "transportation.svg"});
     await db.insert("bill_type", {"name": "Other", "image": "other.svg"});
+
+    AppRepository repo = AppRepository(this);
+
+    var types = await repo.getBillTypes();
+
+    for (var json in testJson) {
+      json['type'] = types[0].toJson();
+      await repo.saveBill(Bill.fromJson(json));
+    }
   }
 
 }
